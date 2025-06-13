ecf3b49167df0d32bcf1c00ff2e81f01556bc9cf
Gaurav <gauravsingh96753@gmail.com>
2025-06-13 06:32:37 +0530

    basic changes for deployment purposes

--- src/pages/PendingDetails/index.tsx
+++ src/pages/PendingDetails/index.tsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 // Example hardcoded data (replace with real fetch logic)
